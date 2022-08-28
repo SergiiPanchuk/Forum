1b15053c7902f1656f330ac1441abad8ac34009b
SergiiPanchuk <sergiip1994@gmail.com>
2022-08-28 13:48:27 +0000

    add carousel

---
+++ js/carousel.js
@@ -0,0 +1,41 @@
+(function (){
+    let position = 0;
+    const slideToShow = 1;
+    const slideToScroll = 1;
+    const container = document.querySelector('.slider__container');
+    const track = document.querySelector('.slider__track');
+    const btnPrev = document.querySelector('.btn__prev');
+    const btnNext = document.querySelector('.btn__next');
+    const items = document.querySelectorAll('.slider__item');
+    const itemsCount = items.length;
+    const itemWidth = container.clientWidth / slideToShow;
+    const movePosition = slideToScroll * itemWidth;
+
+    items.forEach((item) => {
+        item.style.minWidth = `${itemWidth}px`;
+    });
+
+    btnNext.addEventListener('click', () => {
+        const itemsLeft = itemsCount - (Math.abs(position) + slideToShow * itemWidth) / itemWidth;
+
+        position -= itemsLeft >= slideToScroll ? movePosition : itemsLeft * itemWidth;
+        setPosition();
+        checkBtns();
+    });
+
+    btnPrev.addEventListener('click', () => {
+        const itemsLeft = Math.abs(position) / itemWidth;
+
+        position += itemsLeft >= slideToScroll ? movePosition : itemsLeft * itemWidth;
+        setPosition();
+        checkBtns();
+    })
+
+    const setPosition = () => {
+        track.style.transform = `translateX(${position}px)`;
+    }
+    const checkBtns= () => {
+        btnPrev.ariaDisabled = position === 0;
+        btnNext.ariaDisabled = position <= -(itemsCount - slideToShow) * itemWidth;
+    }
+})();
