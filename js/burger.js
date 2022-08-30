3d25bab364b69b17ec6e89ca062e8e65cb0a02ec
Сергій <sergiip1994@gmail.com>
2022-08-30 22:11:08 +0300

    add burger

--- js/burger.js
+++ js/burger.js
@@ -1,10 +1,10 @@
-let menuBtn = document.querySelector('.burger');
-let burgerLinks = document.querySelector('.burger__links');
-menuBtn.addEventListener('click', function () {
-    menuBtn.classList.toggle('active')
-    burgerLinks.classList.toggle('burger__links-active')
+let menuBtn = document.querySelector(".burger");
+let burgerLinks = document.querySelector(".burger__links");
+menuBtn.addEventListener("click", function () {
+  menuBtn.classList.toggle("active");
+  burgerLinks.classList.toggle("burger__links-active");
 });
-burgerLinks.addEventListener('click', function () {
-    menuBtn.classList.remove('active')
-    burgerLinks.classList.remove('burger__links-active')
+burgerLinks.addEventListener("click", function () {
+  menuBtn.classList.remove("active");
+  burgerLinks.classList.remove("burger__links-active");
 });
