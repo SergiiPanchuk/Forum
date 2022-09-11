16e5aefab68804727a2a9340a9bde900261df7ce
Сергій <sergiip1994@gmail.com>
2022-09-11 23:23:08 +0300

    add form and modal-contact

--- js/burger.js
+++ js/burger.js
@@ -1,10 +1,12 @@
-let menuBtn = document.querySelector(".burger");
-let burgerLinks = document.querySelector(".burger__links");
-menuBtn.addEventListener("click", function () {
-  menuBtn.classList.toggle("active");
-  burgerLinks.classList.toggle("burger__links-active");
-});
-burgerLinks.addEventListener("click", function () {
-  menuBtn.classList.remove("active");
-  burgerLinks.classList.remove("burger__links-active");
-});
+(function (){
+	let menuBtn = document.querySelector(".burger");
+  let burgerLinks = document.querySelector(".burger__links");
+  menuBtn.addEventListener("click", function () {
+    menuBtn.classList.toggle("active");
+    burgerLinks.classList.toggle("burger__links-active");
+  });
+  burgerLinks.addEventListener("click", function () {
+    menuBtn.classList.remove("active");
+    burgerLinks.classList.remove("burger__links-active");
+  });
+})();
---
+++ js/modal-form.js
@@ -0,0 +1,31 @@
+(function (){
+	const modal = document.querySelector(".modal");
+	const trigger = document.querySelector(".trigger");
+	const closeButton = document.querySelector(".close-button");
+
+
+	function toggleModal(ev) {
+		modal.classList.toggle("show-modal");
+		ev.stopPropagation();
+	}
+
+	function showModal() {
+		let error = 0;
+		let formReq = document.querySelectorAll('._req')
+		for (let index = 0; index < formReq.length; index++) {
+			const input = formReq[index];
+			if(input.value === ''){
+				error++;
+			}
+		}
+		if(error === 0){
+			toggleModal();
+		}
+		modal.reset();
+	}
+
+	trigger.addEventListener("click", showModal);
+	closeButton.addEventListener("click", toggleModal);
+	modal.addEventListener("click", toggleModal);
+	
+})();
